cdedd733e106a21c1730ec3c58051737ce91d1e0
Tory L Brown <torylbrown@users-mbp.home>
2021-03-25 17:59:00 -0400

    Added app.py and changed tickerData.py

---
+++ app.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import plotly.express as px  # (version 4.7.0)
+import plotly.graph_objects as go
+import tickerData as td
+
+import dash  # (version 1.12.0) pip install dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+app = dash.Dash(__name__)
+
+# ---------- Import and clean data (importing csv into pandas)
+# df = pd.read_csv("intro_bees.csv")
+ticker = 'GRWG'
+df = td.getTickerPriceData(ticker,period='90d',interval='1d')
+signals_df = td.makeTickerDfSignals(df,interval='1d')
+signals_df.drop(columns='Close',inplace=True)
+comb_df = pd.concat([df,signals_df],join='inner',axis=1)
+
+#df = df.groupby(['State', 'ANSI', 'Affected by', 'Year', 'state_code'])[['Pct of Colonies Impacted']].mean()
+#df.reset_index(inplace=True)
+print(comb_df[:5])
+
+def update_graph(data_df,ticker=None):
+
+    dff = data_df.copy()
+    #dff = dff[dff["Year"] == option_slctd]
+    #dff = dff[dff["Affected by"] == "Varroa_mites"]
+    '''
+    # Plotly Express
+    fig = px.choropleth(
+        data_frame=dff,
+        locationmode='USA-states',
+        locations='state_code',
+        scope="usa",
+        color='Pct of Colonies Impacted',
+        hover_data=['State', 'Pct of Colonies Impacted'],
+        color_continuous_scale=px.colors.sequential.YlOrRd,
+        labels={'Pct of Colonies Impacted': '% of Bee Colonies'},
+        template='plotly_dark'
+    )
+    '''
+    # Plotly Graph Objects (GO)
+    fig = go.Figure(
+        data=[go.Candlestick(x=dff.index,
+        open=dff['Open'],
+        high=dff['High'],
+        low=dff['Low'],
+        close=dff['Close'],
+        )]
+    )
+
+    fig.update_layout(
+        title_text="%s Stock Prices"%ticker,
+        title_xanchor="center",
+        title_font=dict(size=24),
+        title_x=0.5,
+    )
+
+    return fig
+
+fig = update_graph(comb_df,ticker)
+# ------------------------------------------------------------------------------
+# App layout
+app.layout = html.Div([
+
+    html.H1("Our Ticker Data with Dash", style={'text-align': 'center'}),
+
+    html.Div(id='none',children=[]),
+
+    dcc.Graph(id='my_ticker_chart', figure=fig)
+
+])
+
+
+# ------------------------------------------------------------------------------
+# Connect the Plotly graphs with Dash Components
+#@app.callback(
+     #[Output(component_id='my_ticker_chart', component_property='figure')],
+    #[Input(component_id='none', component_property='children')]
+#)
+
+
+# ------------------------------------------------------------------------------
+if __name__ == '__main__':
+    app.run_server(debug=True)
--- tickerData.py
+++ tickerData.py
@@ -2,7 +2,8 @@ import yfinance as yf
 import numpy as np
 import scipy as sp
 import pandas as pd
-from talib import RSI,BBANDS,MACD
+#from talib import RSI,BBANDS,MACD
+#from yahoo_fin import stock_info as si
 
 
 def getTickerPriceData(tickers,period='5d',interval='1d'):
@@ -10,14 +11,60 @@ def getTickerPriceData(tickers,period='5d',interval='1d'):
     ticker_df = yf.download(tickers=tickers,period=period,interval=interval)
     return ticker_df
 
+def calculate_macd(data,short_window=8,long_window=22,verbose=0):
+    EMA_short = data.ewm(halflife=short_window).mean()
+    EMA_long = data.ewm(halflife=long_window).mean()
+    macd = EMA_short - EMA_long
+    #data['MACD H'] = macd
+    #data['MACD Signal'] = np.where(df['MACD H'] > 0, 1.0, 0.0)
+    macd_signal = np.where(macd > 0, 1.0, 0.0)
+    if verbose:
+        print(f'The short window is {short_window} & the long window is {long_window}!')
+    return macd,macd_signal
+
+def EWMA(data, ndays):
+    EMA = pd.Series(data['Close'].ewm(span=ndays, min_periods=ndays - 1).mean(),
+                    name='EWMA_' + str(ndays))
+    data = data.join(EMA)
+    return data
+
+def computeRSI(data, time_window):
+    diff = data.diff(1).dropna()  # diff in one field(one day)
+
+    # this preservers dimensions off diff values
+    up_chg = 0 * diff
+    down_chg = 0 * diff
+
+    # up change is equal to the positive difference, otherwise equal to zero
+    up_chg[diff > 0] = diff[diff > 0]
+
+    # down change is equal to negative deifference, otherwise equal to zero
+    down_chg[diff < 0] = diff[diff < 0]
+
+    # we set com=time_window-1 so we get decay alpha=1/time_window
+    up_chg_avg = up_chg.ewm(com=time_window - 1, min_periods=time_window).mean()
+    down_chg_avg = down_chg.ewm(com=time_window - 1, min_periods=time_window).mean()
+
+    rs = abs(up_chg_avg / down_chg_avg)
+    rsi = 100 - 100 / (1 + rs)
+    return rsi
+
+def calculate_BollingerBands(data,long_window=21):
+    ma_long = data.rolling(window=long_window).mean()
+    long_std = data.rolling(window=long_window).std()
+
+    upper = ma_long + (long_std * 2)
+    lower = ma_long - (long_std * 2)
+
+    return upper,lower
+
 def makeTickerDfSignals(ticker_data_df,interval='1d',short_window=9,long_window=21):
     #Add computational signals to the ticker dataframe
 
     # Day Length Trade Intervals:
-    #if ticker_data_df.index.name == 'Date':
+
     signals_df = ticker_data_df.loc[:,['Close']].copy()
-    # Set the `date` column as the index
-    #signals_df.set_index(signals_df.index.name, drop=True, inplace=True)
+
 
     # Calculate Daily Returns
     signals_df['Daily_Return'] = signals_df['Close'].dropna().pct_change()
@@ -26,7 +73,7 @@ def makeTickerDfSignals(ticker_data_df,interval='1d',short_window=9,long_window=
     # Generate the short and long moving averages (short window and long window days, respectively)
     signals_df['SMA%s'%short_window] = signals_df['Close'].rolling(window=short_window).mean()
     signals_df['SMA%s'%long_window] = signals_df['Close'].rolling(window=long_window).mean()
-    #print(signals_df.head())
+
     # Initialize the new `Signal` column
     signals_df['Signal'] = 0.0
 
@@ -50,15 +97,16 @@ def makeTickerDfSignals(ticker_data_df,interval='1d',short_window=9,long_window=
     signals_df["Entry/Exit"] = signals_df["Signal"].diff()
 
     # RSI Indicator
-    signals_df['RSI'] = RSI(ticker_data_df['Close'], timeperiod=14)
+    signals_df['RSI'] = computeRSI(signals_df['Close'], time_window=14)
 
     # MACD Indicator
-    macd, macdsignal, macdhist = MACD(ticker_data_df['Close'], fastperiod=short_window, slowperiod=long_window, signalperiod=6)
+    macd, macdsignal = calculate_macd(signals_df['Close'],short_window=short_window,long_window=long_window)
     signals_df['MACD'] = macd
     signals_df['MACD_Sig'] = macdsignal
 
-    #else:
-        #print("Intra-Day trading will be developed!")
-        #signals_df = [[]]
+    upper_band,lower_band = calculate_BollingerBands(signals_df['Close'], long_window=long_window)
+    signals_df['Upper_Band'] = upper_band
+    signals_df['Lower_Band'] = lower_band
+
 
     return signals_df
